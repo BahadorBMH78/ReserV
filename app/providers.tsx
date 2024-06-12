ec87015ffc6727631a2f37869380fa9dcea2a22d
Bahador Mohammad Hoseini <47@Buluttakin.com>
2024-06-12 11:24:10 +0330

    error handling for 401

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,30 @@
 "use client";
 import { ThemeProvider } from "next-themes";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, signOut } from "next-auth/react";
 import { ReactNode, createContext, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { SeatType } from "@/types/seats";
+import { toast } from "react-toastify";
+import Toast from "@/components/toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      onError: async (error: any) => {
+        if (error.response && error.response.status === 401) {
+          toast(<Toast message="دسترسی غیرمجاز" />, {
+            bodyStyle: {
+              background: "#E2FEE4",
+              border: "1px solid #38F044",
+              borderRadius: "6px",
+              height: 50,
+              fontFamily: "Kalameh",
+            },
+            autoClose: 3000,
+          });
+          signOut({ callbackUrl: "/login", redirect: true });
+        }
+      },
       retry: 0, // retry after 3times error
       suspense: false,
       cacheTime: 0, // 1 day cache time
@@ -33,7 +50,9 @@ export function Providers({ children, session }: Props) {
     <SessionProvider session={session}>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <MyContext.Provider value={{ seats, setSeats, setSelf, self, queue, setQueue }}>
+          <MyContext.Provider
+            value={{ seats, setSeats, setSelf, self, queue, setQueue }}
+          >
             {children}
           </MyContext.Provider>
         </ThemeProvider>
